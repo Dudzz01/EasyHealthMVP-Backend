3ffb677ce4256776fcb7e34d65c4488a2509e6ca
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2025-12-10 23:11:41 -0300

    feat projeto inicial

---
+++ src/main/java/com/mvp/easyhealth/EasyhealthApplication.java
@@ -0,0 +1,13 @@
+package com.mvp.easyhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvp/easyhealth/EasyhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvp.easyhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
